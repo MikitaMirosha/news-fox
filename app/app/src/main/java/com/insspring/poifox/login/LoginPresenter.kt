4a22090b9d28340e30d52e0b4f3c90dc7561e3c1
APridy <anton.pridybailo@gmail.com>
2021-02-21 12:31:35 +0300

    Test

---
+++ app/app/src/main/java/com/insspring/poifox/login/LoginPresenter.kt
@@ -0,0 +1,25 @@
+package com.insspring.poifox.login
+
+import com.arellomobile.mvp.InjectViewState
+import com.delivery.ui.base.BaseMvpPresenter
+
+@InjectViewState
+class LoginPresenter : BaseMvpPresenter<LoginView>() {
+
+    init {
+        viewState.updateImages()
+        viewState.updateEditText()
+        viewState.initOnLoginButton()
+        viewState.updateSignupButton()
+        viewState.initRealm()
+    }
+
+    fun onSignupClicked() {
+        viewState.openSignupActivity()
+    }
+
+//    fun onLoginClicked() {
+//        viewState.openInitialActivity()
+//    }
+
+}
